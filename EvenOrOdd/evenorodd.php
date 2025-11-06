a87799d085e44eb45140aad3d36d670100d34be3
P H Devadathan <phdevadathan001@gmail.com>
2025-11-06 14:15:08 +0530

    Add files via upload

---
+++ EvenOrOdd/evenorodd.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Odd Even</title>
+</head>
+<body>
+<h2>Odd Even Checker</h2>
+<form method="GET" action="<?php echo $_SERVER['_PHP_SELF'];?>">
+Enter a number:<input type="text" name="number">
+<input type="submit" value="Check">
+</form>
+
+
+<?php
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+if(isset($_GET["number"])){
+$number=$_GET["number"];
+if(is_numeric($number)){
+if($number%2==0){
+echo"<p>{$number} is an even number</p>";
+}
+else{
+echo"<p>{$number} is an odd number.</p>";
+}
+}
+else{
+echo"<p>Please enter a valid number</p>";
+}
+}
+}
+?>
+</body>
+</html>
